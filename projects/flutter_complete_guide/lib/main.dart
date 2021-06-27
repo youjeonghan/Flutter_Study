37c17b8072d8530edfd27517a58154bf2ae0e60b
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-06-28 00:24:47 +0900

    33. Connecting Functions & Buttons

--- projects/flutter_complete_guide/lib/main.dart
+++ projects/flutter_complete_guide/lib/main.dart
@@ -7,8 +7,39 @@ void main() {
 // void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello!"));
+    var questions = [
+      "What\'s your favorite color?",
+      "What\'s your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text("The Question!"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
