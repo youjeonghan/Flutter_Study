b6bf2ace8e0f07c4f1cfef5304329634497c518c
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-04 23:55:37 +0900

    2-37. Updating Correctly with Stateful Widgets

--- projects/flutter_complete_guide/lib/main.dart
+++ projects/flutter_complete_guide/lib/main.dart
@@ -6,10 +6,21 @@ void main() {
 
 // void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    // throw UnimplementedError();
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
   }
 
