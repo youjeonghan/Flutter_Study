fe21572ed10f9bf26a24ef05ce08e861fecc1647
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-06 15:08:43 +0900

    2-44. Introducing Maps

--- projects/flutter_complete_guide/lib/main.dart
+++ projects/flutter_complete_guide/lib/main.dart
@@ -32,8 +32,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      "What\'s your favorite color?",
-      "What\'s your favorite animal?",
+      {
+        "questionText": "What\'s your favorite color?",
+        "answer": ["Black", "Red", "Green", "White"],
+      },
+      {
+        "questionText": "What\'s your favorite animal?",
+        "answer": ["Rabbit", "Snake", "Elephant", "Lion"],
+      },
+      {
+        "questionText": "Who\'s your favorite teacher?",
+        "answer": ["Max", "Max", "Max", "Max"],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
