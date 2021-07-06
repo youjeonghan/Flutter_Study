f98e75c5b1270105f451243a7e3e087ed1896a18
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-07 00:49:17 +0900

    2-53. Getters & "else-if"

---
+++ dart_study/lib/test.dart
@@ -0,0 +1,5 @@
+void main() {
+  int a = 10;
+  String s = "You are score $a";
+  print(s);
+}
--- projects/flutter_complete_guide/lib/main.dart
+++ projects/flutter_complete_guide/lib/main.dart
@@ -24,23 +24,41 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       "questionText": "What\'s your favorite color?",
-      "answer": ["Black", "Red", "Green", "White"],
+      "answer": [
+        {"text": "Black", "score": 10},
+        {"text": "Red", "score": 5},
+        {"text": "Green", "score": 3},
+        {"text": "White", "score": 1},
+      ],
     },
     {
       "questionText": "What\'s your favorite animal?",
-      "answer": ["Rabbit", "Snake", "Elephant", "Lion"],
+      "answer": [
+        {"text": "Rabbit", "score": 3},
+        {"text": "Snake", "score": 11},
+        {"text": "Elephant", "score": 5},
+        {"text": "Lion", "score": 9},
+      ],
     },
     {
       "questionText": "Who\'s your favorite instructor?",
-      "answer": ["Max", "Max", "Max"],
+      "answer": [
+        {"text": "Max", "score": 1},
+        {"text": "Max", "score": 1},
+        {"text": "Max", "score": 1},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
     // setState
     // 해당 함수가 들어있는 위젯만 다시 빌드함
     // 그래서 변경사항이 수정됨
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -67,7 +85,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
