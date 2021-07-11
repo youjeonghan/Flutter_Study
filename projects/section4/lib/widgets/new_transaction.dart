26f2c71d0ba44a4c80be5f55f8db6bd38747a589
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-11 16:52:42 +0900

    4-91. Further Input & Output Styling and Configuration

--- projects/section4/lib/widgets/new_transaction.dart
+++ projects/section4/lib/widgets/new_transaction.dart
@@ -7,6 +7,16 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,18 +28,17 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "Title"),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: "Amount"),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              //keyboardType: TextInputType.numberWithOptions(decimal: true), // IOS용
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
               textColor: Colors.purple,
               child: Text("Add Transaction"),
             )
