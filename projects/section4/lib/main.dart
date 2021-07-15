dd52d5ce2b2a2f339ba170dc70b446b161decc52
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-07-15 14:21:21 +0900

    4-97. Adding Images to the App

--- projects/section4/lib/main.dart
+++ projects/section4/lib/main.dart
@@ -48,18 +48,18 @@ class _MyHomePageState extends State<MyHomePage> {
   final amountController = TextEditingController();
 
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: "t1",
-      title: "New Shoes",
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t2",
-      title: "Weekly Groceries",
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: "t1",
+    //   title: "New Shoes",
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "t2",
+    //   title: "Weekly Groceries",
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addNewtransaction(String txTitle, double txAmount) {
--- projects/section4/lib/widgets/transaction_list.dart
+++ projects/section4/lib/widgets/transaction_list.dart
@@ -11,7 +11,17 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 800,
-      child: ListView.builder(
+      child: transactions.isEmpty ? Column(
+        children: <Widget>[
+          Text("No transactions added yet!",style:Theme.of(context).textTheme.title,),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 200,
+            child: Image.asset("assets/images/waiting.png", fit: BoxFit.cover,)),
+        ],
+      ) : ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
             child: Row(
